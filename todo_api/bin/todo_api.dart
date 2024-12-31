ef973accc702c1d225011c25701ef6d6c5f4b7db
matu <matu@a.com>
2024-12-31 17:30:50 +0900

    update: サーバ起動、エコー用ハンドラー等追加

--- todo_api/bin/todo_api.dart
+++ todo_api/bin/todo_api.dart
@@ -1,5 +1,20 @@
-import 'package:todo_api/todo_api.dart' as todo_api;
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as shelf_io;
 
-void main(List<String> arguments) {
-  print('Hello world: ${todo_api.calculate()}!');
+void main(List<String> arguments) async {
+  // リクエストを処理するハンドラー追加
+  // ミドルウェア追加
+  final handler = const Pipeline().addMiddleware(logRequests()).addHandler(_echoRequest);
+
+  // サーバ起動
+  var server = await shelf_io.serve(handler, 'localhost', 8080);
+
+  // コンテンツ圧縮機能ON(レスポンス速度が上がる)
+  server.autoCompress = true;
+
+  // サーバ起動メッセージ
+  print('Serving at http://${server.address.host}:${server.port}');
 }
+
+// リクエストを処理するハンドラー
+Response _echoRequest(Request request) => Response.ok('Request for "${request.url}"');
